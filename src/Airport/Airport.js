94a68839b324e6a254db6001e2a87e7f2ab6c3ac
Byeonghee-son <0722ths@gmail.com>
2023-07-25 15:35:45 +0900

    0725

--- src/Airport/Airport.js
+++ src/Airport/Airport.js
@@ -1,25 +1,38 @@
 import Mapsearch from './Mapsearch';
-
+import airportlogo from '../img/airportlogo.png'
 
 
 const Airport = () => {
    
     return (
         <>  
-            <nav>
-                <ul>
-                    <li><strong>세계공항정보</strong></li>
-                </ul>
-                <ul>
-                    <li><a href="#"></a>아시아</li>
-                    <li><a href="#"></a>유럽</li>
-                    <li><a href="#"></a>북아메리카</li>
-                    <li><a href="#"></a>남아메리카</li>
-                    <li><a href="#"></a>중동</li>
-                    <li><a href="#"></a>아프리카</li>
-                    <li><a href="#"></a>대양주</li>
-                </ul>
-            </nav>
+            <header className="flex">
+                <nav>
+                    <ul>
+                        <li><img className='airportlogo' src={airportlogo} alt="Airportlogo" /></li>
+                    </ul>
+                    <ul>
+                        <li><strong>세계 공항 포털</strong></li>
+                    </ul>
+                    <ul>
+                        <li><h3>로그인</h3></li>
+                        <li><h3>회원가입</h3></li>
+                    </ul>
+                </nav>
+            </header>
+            <body>
+                <nav>
+                    <ul>
+                        <li>사이트 정보</li>
+                        <li>지도api</li>
+                        <li>공항 정보</li>
+                        <li>자유 게시판</li>
+                    </ul>
+                    <ul>
+                        <input type="text" />
+                    </ul>
+                </nav>
+            </body>
         </>
     );
 }
