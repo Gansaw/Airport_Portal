51753afb08d206457e7e85cb1461317a6dab279b
Byeonghee-son <0722ths@gmail.com>
2023-08-17 10:47:50 +0900

    se

--- src/Main/Searchtool.js
+++ src/Main/Searchtool.js
@@ -12,29 +12,29 @@ const Searchtool = () => {
     };
 
     const handleSubmit = (e) => {
-        e.preventDefault();
+        e.preventDefault();        
         navigate(`/airportInfo?search=${encodeURIComponent(searchValue)}`);
     };
 
     return (
-        <form className={style.form} onSubmit={handleSubmit} method='get'>
-            <fieldset className={style.fieldset}>
-                <div className={style.searchdiv1}>
-                    <input
-                        type="search"
-                        className={style.searchtool}
-                        id='search'
-                        placeholder='국가 또는 대륙 또는 공항코드를 입력하세요'
-                        value={searchValue}
-                        onChange={handleSearchChange}
-                    />
-                </div>
-                <div className={style.searchdiv2}>
-                    <button type="submit" className={style.searchbt}>검색</button>
-                </div>
-            </fieldset>
-        </form>
-
+         <form onSubmit={handleSubmit} method='get'>
+                    <fieldset className={style.fieldset}>
+                        <div className={style.searchdiv1}>
+                            <input
+                                type="search"
+                                className={style.searchtool}
+                                id='search'
+                                placeholder='국가 또는 대륙 또는 공항코드를 입력하세요'
+                                value={searchValue}
+                                onChange={handleSearchChange}
+                            />
+                        </div>
+                        <div className={style.searchdiv2}>
+                            <button type="submit" className={style.searchbt}>검색</button>
+                        </div>
+                    </fieldset>
+                </form>
+            
     );
 };
 
