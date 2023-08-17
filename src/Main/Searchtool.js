905d429b477bf76d319cfc04e563eba470081779
Byeonghee-son <0722ths@gmail.com>
2023-08-17 09:33:38 +0900

    search

--- src/Main/Searchtool.js
+++ src/Main/Searchtool.js
@@ -1,44 +1,33 @@
 import style from '../Airport/Airport.module.css';
 import './Maincss.css';
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React,{useState} from 'react';
 
-const Searchtool = () => {
-    const [searchValue, setSearchValue] = useState('');
-    const navigate = useNavigate();
-
+const Searchtool = () =>{
+    const [searchValue, setSearchValue] = useState('');    
     const handleSearchChange = (e) => {
         setSearchValue(e.target.value);
     };
 
     const handleSubmit = (e) => {
-        e.preventDefault();        
-        navigate(`/airportInfo?search=${encodeURIComponent(searchValue)}`);
-    };
+        e.preventDefault();
+        window.location.href = `/mapsearch?search=${encodeURIComponent(searchValue)}`;
+    }; 
 
-    return (
-        <>
-            <div className={style.rt}>
-                <form className={style.form} onSubmit={handleSubmit} method='get'>
-                    <fieldset className={style.fieldset}>
-                        <div className={style.searchdiv1}>
-                            <input
-                                type="search"
-                                className={style.searchtool}
-                                id='search'
-                                placeholder='국가 또는 대륙 또는 공항코드를 입력하세요'
-                                value={searchValue}
-                                onChange={handleSearchChange}
-                            />
-                        </div>
-                        <div className={style.searchdiv2}>
-                            <button type="submit" className={style.searchbt}>검색</button>
-                        </div>
-                    </fieldset>
-                </form>
-            </div>
-        </>
-    );
-};
-
+    
+    return(
+        <form onSubmit={handleSubmit} method='get' className={style.rt}>
+            <fieldset className={style.fieldset}>
+                <input 
+                    type="search" 
+                    className={style.searchtool} 
+                    id='search' 
+                    placeholder='국가 또는 대륙 또는 공항코드를 입력하세요' 
+                    value={searchValue} 
+                    onChange={handleSearchChange}
+                />
+                <button type="submit" className={style.searchbt}>검색</button>
+            </fieldset>
+        </form>  
+    )
+}
 export default Searchtool;
